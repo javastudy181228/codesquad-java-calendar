feb0c12ce5fb02d7b82356ae93ad0e9b4b55982c
apple <apple@DESKTOP-5MNI6HN>
2019-01-03 15:39:04 +0900

    Add reafeat function

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -8,6 +8,7 @@ public class Calendar {
 	public int getMaxDaysOfMonth(int month){
 		return MAX_DAYS[month-1];
 	}
+	
 	public void printSampleCalendar(){
 		System.out.println("일     월      화     수     목     금    토 ");
 		System.out.println("--------------------");
@@ -16,14 +17,20 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
+	
 	public static void main(String[] args){
 		//숫자를 입력받아 행당하는  달의 회대 일수
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
 		
-		System.out.println(month+"달은"+cal.getMaxDaysOfMonth(month)+"일까지 있습니다.");
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		for(int i=0; i<repeat; i++){
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.println(month+"달은"+cal.getMaxDaysOfMonth(month)+"일까지 있습니다.");
+		}
+		System.out.println("Bye~~");
 		cal.printSampleCalendar();
 		scanner.close();
 	}
